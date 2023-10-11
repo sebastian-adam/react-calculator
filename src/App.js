2bb47b3943a77e65fe89a8a66d84c9fe1571e88a
@sebastian.adam <sebastian.adam.work@gmail.com>
2023-10-11 13:53:16 -0500

    Lint App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import './App.css';
+import "./App.css";
 
-import { useState } from 'react';
-import Button from './Button';
+import { useState } from "react";
+import Button from "./Button";
 
 const buttons = [
-  "AC", "+/-", "%", "÷",
-  "7", "8", "9", "x",
-  "4", "5", "6", "-",
-  "1", "2", "3", "+",
-  "0", ".", "="];
+  "AC",
+  "+/-",
+  "%",
+  "÷",
+  "7",
+  "8",
+  "9",
+  "x",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
 
 function App() {
-
   // You can use this to maintain the calculator buffer
   const [calcBuffer, setCalcBuffer] = useState([]);
 
   // Handle button clicks here
-  function handleClick(value) {
-   
-  }
+  function handleClick(value) {}
 
   return (
     <div className="App">
@@ -31,7 +43,7 @@ function App() {
         <div className="display">0</div>
         <div className="keypad">
           {buttons.map((button, index) => {
-            return <Button label={button} handleClick={handleClick} />
+            return <Button label={button} handleClick={handleClick} />;
           })}
         </div>
       </main>
