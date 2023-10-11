9d2a9d403e8db8da10129f675888c062ab12aee2
@sebastian.adam <sebastian.adam.work@gmail.com>
2023-10-11 17:14:25 -0500

    Do not allow user to chain operators

--- src/App.js
+++ src/App.js
@@ -96,29 +96,32 @@ function App() {
     const lastValue = equation[equation.length - 1];
     const valuesBeforeLastValue = equation.slice(0, -1);
 
-    // ..if the last value in the "equation" array is undefined...
+    // ..if the equation is unitialized..
     if (equation.length === 0) {
-      // ...and the next value is a string, add it to array
+      // ...and the first value is a string, add the digit as a new item on array.
       if (typeof nextValue === "string") {
         setEquation([nextValue]);
       }
-      // ...and the next value is not a string, do nothing
+      // (if the next value is not a string, do nothing)
+      // else if the equation is already initialized..
     } else {
-      // ..if the last value in the "equation" array and the next value are unalike in type, append next value to array
+      // if the last value in the "equation" array and the next value are unalike in type, add new item to array.
       if (typeof lastValue !== typeof nextValue) {
         setEquation([...equation, nextValue]);
-        // ..if the last value in the "equation" array is a string, and the next value is a string, combine them into one string
-      } else if (
-        typeof lastValue === "string" &&
-        typeof nextValue === "string"
-      ) {
+      }
+
+      // ..if the last value in the "equation" array is a string, and the next value is a string, combine them into one number.
+      if (typeof lastValue === "string" && typeof nextValue === "string") {
         setEquation([...valuesBeforeLastValue, `${lastValue}${nextValue}`]);
+      }
+
+      // ..if the last value in the "equation" array is an operator, and the next value is an operator, overwrite previous operator.
+      if (lastValue.operator && nextValue.operator) {
+        setEquation([...valuesBeforeLastValue, nextValue]);
       }
     }
 
     // ..if the the next value is a a submission operator, reduce (to single item)
-
-    // ..if the last value in the "equation" array is an operator, and the next value is an operator, replace
   }
 
   // Presentation layer
